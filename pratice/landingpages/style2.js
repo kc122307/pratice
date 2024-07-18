33d9e74d4069126b6ae78fe6d721918462c2f247
kc12230 <chandelshunu@gmail.com>
2024-07-18 16:27:57 +0530

    project added

---
+++ pratice/landingpages/style2.js
@@ -0,0 +1,18 @@
+gsap.from("#img1",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 0.8,
+    y:60
+    })
+  gsap.from("#img2",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 0.8,
+    x:60
+  })
+  gsap.from("#img3",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 0.8,
+    y:-60
+  })
